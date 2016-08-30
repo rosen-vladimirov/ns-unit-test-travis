327c62f5c70f3eefd1e0c9c5a86871ee768c354f
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-08-30 08:09:26 +0300

    Add more fake unit tests

--- unit-test-proj/app/tests/example.js
+++ unit-test-proj/app/tests/example.js
@@ -3,4 +3,12 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+
+   it("contains spec with an expectation 2", function() {
+    expect(false).toBe(false);
+  });
+
+   it("contains spec with an expectation 3", function() {
+    expect(true).toBe(true);
+  });
 });
